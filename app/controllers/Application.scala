673f3cc618ec778ab57ef8571efe7fb939bd5d65
Arseni <arsenius75@gmail.com>
2016-05-16 00:44:32 +0300

    refactoring

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,34 +14,16 @@ class Application extends Controller{
     Ok(views.html.index("Hello World!"))
   }
 
-
-  val personForm: Form[Person] = Form {
-    mapping(
-      "name" -> text
-    )(Person.apply)(Person.unapply)
-  }
-
   val searchForm: Form[Hashtag] = Form {
     mapping(
       "hashtag" -> text
     )(Hashtag.apply)(Hashtag.unapply)
   }
 
-  def addPerson() = Action { implicit request =>
-    val person = personForm.bindFromRequest.get
-    DB.save(person)
-    Redirect(routes.Application.index)
-  }
-
   def SearchByHashtag() = Action { implicit request =>
     val hashtag = searchForm.bindFromRequest.get
 
     Redirect(routes.Application.getTweets(hashtag.hashtag))
-  }
-
-  def getPersons = Action {
-    val persons = DB.query[Person].fetch()
-    Ok(Json.toJson(persons))
   }
 
   def getTweets(hashtag: String) = Action {
